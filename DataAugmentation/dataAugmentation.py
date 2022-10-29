9e9372e201e4c12161bd346ccde06a4272072f57
Lavanya S <64472461+Lavanyashankar09@users.noreply.github.com>
2022-10-29 15:37:37 +0530

    Add files via upload

---
+++ DataAugmentation/dataAugmentation.py
@@ -0,0 +1,24 @@
+from keras_preprocessing.image import ImageDataGenerator, img_to_array, load_img
+
+img = load_img('flower.jpg')  
+x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
+x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
+
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+i = 0
+for batch in datagen.flow(x, batch_size=1,
+                          save_to_dir='preview', save_prefix='flower', save_format='jpeg'):
+    i += 1
+    if i > 2:
+        break  # otherwise the generator would loop indefinitely
